8049eae40111514232992c84cc252da7a6950cbc
ManiackMike <105581753+ManiackMike@users.noreply.github.com>
2023-11-15 17:27:03 +0100

    Add files via upload

---
+++ Contattami_files/Contattami.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.1 local-build-20231114
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,200),url:'Contattami_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'Contattami_files/stroke_1.png'},{rect:new IWRect(1,-1,200,2),url:'Contattami_files/stroke_2.png'},{rect:new IWRect(201,-1,2,2),url:'Contattami_files/stroke_3.png'},{rect:new IWRect(201,1,2,200),url:'Contattami_files/stroke_4.png'},{rect:new IWRect(201,201,2,2),url:'Contattami_files/stroke_5.png'},{rect:new IWRect(1,201,200,2),url:'Contattami_files/stroke_6.png'},{rect:new IWRect(-1,201,2,2),url:'Contattami_files/stroke_7.png'}],new IWSize(202,202)),shadow_0:new IWShadow({blurRadius:2,offset:new IWPoint(1.0000,0.0000),color:'#3a060f',opacity:1.000000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Contattami_files/ContattamiMoz.css')
+fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();fixupIECSS3Opacity('id1');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
