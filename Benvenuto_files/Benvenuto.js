6830ca33c1fc5c27c69ccb8b884e6a07cd9ce37a
ManiackMike <105581753+ManiackMike@users.noreply.github.com>
2023-11-15 17:27:50 +0100

    Add files via upload

---
+++ Benvenuto_files/Benvenuto.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.1 local-build-20231115
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:2,offset:new IWPoint(1.0000,0.0000),color:'#3a060f',opacity:1.000000}),stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,42),url:'Benvenuto_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'Benvenuto_files/stroke_1.png'},{rect:new IWRect(1,-1,124,2),url:'Benvenuto_files/stroke_2.png'},{rect:new IWRect(125,-1,2,2),url:'Benvenuto_files/stroke_3.png'},{rect:new IWRect(125,1,2,42),url:'Benvenuto_files/stroke_4.png'},{rect:new IWRect(125,43,2,2),url:'Benvenuto_files/stroke_5.png'},{rect:new IWRect(1,43,124,2),url:'Benvenuto_files/stroke_6.png'},{rect:new IWRect(-1,43,2,2),url:'Benvenuto_files/stroke_7.png'}],new IWSize(126,44)),stroke_1:new IWStrokeParts([{rect:new IWRect(-2,2,4,244),url:'Benvenuto_files/stroke_8.png'},{rect:new IWRect(-2,-2,4,4),url:'Benvenuto_files/stroke_9.png'},{rect:new IWRect(2,-2,167,4),url:'Benvenuto_files/stroke_10.png'},{rect:new IWRect(169,-2,4,4),url:'Benvenuto_files/stroke_11.png'},{rect:new IWRect(169,2,4,244),url:'Benvenuto_files/stroke_12.png'},{rect:new IWRect(169,246,4,4),url:'Benvenuto_files/stroke_13.png'},{rect:new IWRect(2,246,167,4),url:'Benvenuto_files/stroke_14.png'},{rect:new IWRect(-2,246,4,4),url:'Benvenuto_files/stroke_15.png'}],new IWSize(171,248))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Benvenuto_files/BenvenutoMoz.css')
+adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();fixupIECSS3Opacity('id1');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
